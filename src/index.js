ae69be54b2c851a57abb7c282d8aa3fb4d20d639
Cryothermus <evangrvy@gmail.com>
2020-05-21 00:09:10 -0700

    Deleting crap, adding first bits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Item extends React.Component {
+  
+}
+
+class ItemList extends React.Component {
+
+}
+
+class AddItem extends React.Component {
+ render() {
+   return (
+    <div>
+      <input className="inputBox" type="text"></input>
+      <input type="submit"></input>
+    </div>
+   );
+ }
+}
+
+class List extends React.Component {
+  render() {
+    return (
+      <AddItem />
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <List />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
