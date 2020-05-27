cf9c6dec5f19aee57a149bc72878834283196c5a
Cryothermus <evangrvy@gmail.com>
2020-05-26 19:28:59 -0700

    added proper documentation

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 class Item extends React.Component {
+//class for individual items on the list
 render () {
   return (
+    //contains the item as a whole, its text, and its delete button
     <div className="item">
         <p 
         className="itemText"
         >
         {this.props.text}
         </p>
-
         <input 
         className="delButton" 
         type="button" 
@@ -22,15 +23,16 @@ render () {
 }
 }
 
-class ItemList extends React.Component { //TODO: add keys
-
+class ItemList extends React.Component {
+//for the list of items, containing each individual item
   render() {
+    //creates each item component from the main array
     var listContent = this.props.items.map((item, index) => <Item
      text={item} 
      key={item}
      onDelete={(i) => this.props.onRemove(index)}
      ></Item>);
-
+    //renders the resulting components in a list
     return (
       <ul id="itemList">{listContent}</ul>
     )
@@ -38,7 +40,7 @@ class ItemList extends React.Component { //TODO: add keys
 }
 
 class AddItem extends React.Component {
-
+//handles the form to add to the list- text box + submit button
  render() {
    return (
     <form id="addItem" onSubmit={this.props.onSubmit}>
@@ -48,7 +50,6 @@ class AddItem extends React.Component {
       onChange={this.props.onChange}
       autoComplete="off">
       </input>
-
       <input id="itemSubmit" type="submit" value="Submit"></input>
     </form>
    );
@@ -56,7 +57,9 @@ class AddItem extends React.Component {
 }
 
 class List extends React.Component {
-  //contains form content and list of items
+  //the app as a whole- a list of strings that can be added to and removed
+
+  //constructor contains form content and list of items
   constructor(props) {
     super(props);
     this.state = {
@@ -69,27 +72,30 @@ class List extends React.Component {
   this.onDelete = this.onDelete.bind(this);
 
   }
-  //handles text input into entry box
+  //handles manipulation of the input text box
     onChange(event) {
       this.setState({formContent: event.target.value});
     }
-    //adds items to the list
+
+    //adds items to the list upon submission
     onSubmit(event) {
       if (this.state.formContent !== '') {
         this.setState({itemList: [...this.state.itemList, this.state.formContent]})
         console.log('Submitted: ' + this.state.formContent);
-        this.setState({formContent: ''});
+        this.setState({formContent: ''}); //clears the text box upon submission
       }
       event.preventDefault();
     }
-    //removes items from the list
+    //removes items from the list upon clicking the appropriate X button
     onDelete(index) {
       var array = [...this.state.itemList];
       array.splice(index, 1);
       this.setState({itemList: array});
+      console.log('Removed item:' + index)
     }
-
+  
   render() {
+    //the rendering function proper- renders the input form and the list
     return (
       <div id="list">
         <div>
